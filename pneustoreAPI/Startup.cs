3895daad045fffcd0fcc6a3df3626b326b0404e3
Caio Ferrari <caioferrari0484@gmail.com>
2021-09-23 19:45:04 -0300

    Corrigindo erro do CORS

--- pneustoreAPI/Startup.cs
+++ pneustoreAPI/Startup.cs
@@ -23,8 +23,9 @@ namespace pneustoreAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "pneustoreAPI", Version = "v1" });
@@ -49,6 +50,11 @@ namespace pneustoreAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseAuthorization();
 
