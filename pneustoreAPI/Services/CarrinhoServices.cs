81a0714c061c682c9efdce1c2716325e60bb480f
Pedro Henrique <81339296+kindbiscuit@users.noreply.github.com>
2021-09-30 22:34:47 -0300

    implementação do metodo delete no carrinhoServices

--- pneustoreAPI/Services/CarrinhoServices.cs
+++ pneustoreAPI/Services/CarrinhoServices.cs
@@ -1,4 +1,5 @@
-﻿using pneustoreAPI.Data;
+﻿using Microsoft.AspNetCore.Identity;
+using pneustoreAPI.Data;
 using pneustoreAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,11 @@ namespace pneustoreAPI.Services
     public class CarrinhoServices : IService<Carrinho>
     {
         Context context;
-        public CarrinhoServices(Context context)
+        public readonly UserManager<PneuUser> userManager;
+        public CarrinhoServices(Context context, UserManager<PneuUser> _userManager)
         {
             this.context = context;
+            userManager = _userManager;
         }
         public bool Create(Carrinho objeto)
         {
@@ -28,5 +31,19 @@ namespace pneustoreAPI.Services
         {
             throw new NotImplementedException();
         }
+        public bool Delete(int? id)
+        {
+            try
+            {
+                context.Products.Remove(Get(id));
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
     }
 }
