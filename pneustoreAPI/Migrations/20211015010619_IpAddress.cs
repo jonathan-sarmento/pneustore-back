73b527fbe26e49b32d0dff81c2ad8a4616ffbbdb
Caio Ferrari <caioferrari0484@gmail.com>
2021-10-14 23:03:24 -0300

    Corrigindo erros na criação de usuário anonimo; implementação completa da migração de carrinhos

---
+++ pneustoreAPI/Migrations/20211015010619_IpAddress.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pneustoreAPI.Migrations
+{
+    public partial class IpAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IP",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IP",
+                table: "AspNetUsers");
+        }
+    }
+}
